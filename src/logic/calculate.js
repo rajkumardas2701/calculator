0013280a5d2ee972734032eae67fad3dd3336ee4
rajkumardas2701 <41339878+rajkumardas2701@users.noreply.github.com>
2021-02-16 20:37:10 +0530

    Add condition for other operators in calculate.js

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,11 +1,28 @@
+import calc from './operate';
+
 export default function calculate(calData, btnName) {
   let { total, next, operation } = calData;
+  const operators = ['+', '-', '/', '*'];
 
   if (btnName === '+/-') {
     total *= -1;
     next *= -1;
   } else if (btnName === 'AC') {
     total = 0;
+    next = null;
+    operation = null;
+  } else if (btnName === '%') {
+    total = calc(total, next, operation);
+    next = null;
+    operation = null;
+  } else if (btnName === '.' && next) {
+    if (!next.includes('.')) {
+      next += '.';
+    }
+  } else if (operators.includes(btnName)) {
+    calc(total, next, operation);
+  } else if ((btnName === '=') && (next && total)) {
+    total = calc(total, next, operation);
     next = null;
     operation = null;
   }
