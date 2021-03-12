eed2c3b0185da20d1603c1201d798d9882e10f06
rajkumardas2701 <41339878+rajkumardas2701@users.noreply.github.com>
2021-03-12 15:46:28 +0530

    Remove eslint comment

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -45,8 +45,3 @@ export default function ButtonPanel({ clickHandler }) {
 ButtonPanel.propTypes = {
   clickHandler: PropTypes.func.isRequired,
 };
-
-// eslint-disable-next-line no-unused-vars
-const handleClick = btnName => {
-  this.props.clickHandler(btnName);
-};
