40ff16ae5cf07cdf7eacc22f0f9c3cf563840449
rajkumardas2701 <41339878+rajkumardas2701@users.noreply.github.com>
2021-03-22 01:46:25 +0530

    Refactor App Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: '0',
-      next: null,
-      operation: null,
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
+const App = () => {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     total: '0',
+  //     next: null,
+  //     operation: null,
+  //   };
+  //   this.handleClick = this.handleClick.bind(this);
+  // }
 
-  handleClick(btnName) {
-    const { total, next, operation } = this.state;
+  const [total, setTotal] = useState(0);
+  const [next, setNext] = useState(null);
+  const [operation, setOperation] = useState(null);
+
+  const handleClick = btnName => {
+    // const { total, next, operation } = this.state;
     const calData = calculate({ total, next, operation }, btnName);
-    this.setState({
-      total: calData.total,
-      next: calData.next,
-      operation: calData.operation,
-    });
-  }
+    // this.setState({
+    //   total: calData.total,
+    //   next: calData.next,
+    //   operation: calData.operation,
+    // });
 
-  render() {
-    const { next, total, operation } = this.state;
-    return (
-      <div className="app">
-        <Display total={total} next={next} operation={operation} />
-        <ButtonPanel clickHandler={btnName => this.handleClick(btnName)} />
-      </div>
-    );
-  }
-}
+    setTotal(calData.total);
+    setNext(calData.next);
+    setOperation(calData.operation);
+  };
+
+  // render() {
+  //   const { next, total, operation } = this.state;
+  return (
+    <div className="app">
+      <Display total={total} next={next} operation={operation} />
+      <ButtonPanel clickHandler={btnName => handleClick(btnName)} />
+    </div>
+  );
+};
+// }
+
+export default App;
