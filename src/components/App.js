d0f1de47ecf7e5ae5cfc464a32e1434f8980c39b
rajkumardas2701 <41339878+rajkumardas2701@users.noreply.github.com>
2021-03-22 02:20:21 +0530

    Remove Comments

--- src/components/App.js
+++ src/components/App.js
@@ -4,36 +4,17 @@ import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
 
 const App = () => {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     total: '0',
-  //     next: null,
-  //     operation: null,
-  //   };
-  //   this.handleClick = this.handleClick.bind(this);
-  // }
-
   const [total, setTotal] = useState(0);
   const [next, setNext] = useState(null);
   const [operation, setOperation] = useState(null);
 
   const handleClick = btnName => {
-    // const { total, next, operation } = this.state;
     const calData = calculate({ total, next, operation }, btnName);
-    // this.setState({
-    //   total: calData.total,
-    //   next: calData.next,
-    //   operation: calData.operation,
-    // });
-
     setTotal(calData.total);
     setNext(calData.next);
     setOperation(calData.operation);
   };
 
-  // render() {
-  //   const { next, total, operation } = this.state;
   return (
     <div className="app">
       <Display total={total} next={next} operation={operation} />
@@ -41,6 +22,5 @@ const App = () => {
     </div>
   );
 };
-// }
 
 export default App;
