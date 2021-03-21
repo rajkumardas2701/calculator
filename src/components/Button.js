18959ac7a13a5b5f253d37a46eab434ed170fa1f
rajkumardas2701 <41339878+rajkumardas2701@users.noreply.github.com>
2021-03-21 05:51:31 +0530

    Add styles to the components

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function Button({ name, handleClick }) {
+export default function Button({
+  name, handleClick, color, wide = false,
+}) {
   return (
-    <button type="button" onClick={() => handleClick(name)}>
+    <button type="button" onClick={() => handleClick(name)} className={wide ? 'zeroBtn' : 'otherBtn'} style={{ background: color }}>
       {name}
     </button>
   );
 }
 
+Button.defaultProps = {
+  color: 'orange',
+};
+
 Button.propTypes = {
   name: PropTypes.string.isRequired,
   handleClick: PropTypes.func.isRequired,
+  color: PropTypes.string,
+  wide: PropTypes.bool.isRequired,
 };
--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -6,36 +6,36 @@ export default function ButtonPanel({ clickHandler }) {
   return (
     <div className="groups">
       <div className="G1">
-        <Button name="AC" handleClick={clickHandler} />
-        <Button name="+/-" handleClick={clickHandler} />
-        <Button name="%" handleClick={clickHandler} />
+        <Button name="AC" handleClick={clickHandler} color="#EFEFEF" />
+        <Button name="+/-" handleClick={clickHandler} color="#EFEFEF" />
+        <Button name="%" handleClick={clickHandler} color="#EFEFEF" />
         <Button name="/" handleClick={clickHandler} />
       </div>
 
       <div className="G2">
-        <Button name="7" handleClick={clickHandler} />
-        <Button name="8" handleClick={clickHandler} />
-        <Button name="9" handleClick={clickHandler} />
+        <Button name="7" handleClick={clickHandler} color="#EFEFEF" />
+        <Button name="8" handleClick={clickHandler} color="#EFEFEF" />
+        <Button name="9" handleClick={clickHandler} color="#EFEFEF" />
         <Button name="*" handleClick={clickHandler} />
       </div>
 
       <div className="G3">
-        <Button name="4" handleClick={clickHandler} />
-        <Button name="5" handleClick={clickHandler} />
-        <Button name="6" handleClick={clickHandler} />
+        <Button name="4" handleClick={clickHandler} color="#EFEFEF" />
+        <Button name="5" handleClick={clickHandler} color="#EFEFEF" />
+        <Button name="6" handleClick={clickHandler} color="#EFEFEF" />
         <Button name="-" handleClick={clickHandler} />
       </div>
 
       <div className="G4">
-        <Button name="1" handleClick={clickHandler} />
-        <Button name="2" handleClick={clickHandler} />
-        <Button name="3" handleClick={clickHandler} />
+        <Button name="1" handleClick={clickHandler} color="#EFEFEF" />
+        <Button name="2" handleClick={clickHandler} color="#EFEFEF" />
+        <Button name="3" handleClick={clickHandler} color="#EFEFEF" />
         <Button name="+" handleClick={clickHandler} />
       </div>
 
       <div className="G5">
-        <Button name="0" handleClick={clickHandler} />
-        <Button name="." handleClick={clickHandler} />
+        <Button name="0" wide handleClick={clickHandler} color="#EFEFEF" />
+        <Button name="." handleClick={clickHandler} color="#EFEFEF" />
         <Button name="=" handleClick={clickHandler} />
       </div>
     </div>
