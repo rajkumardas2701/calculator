4b20e71fe5ec9324db1c8d6c44f8dcd87d7af52c
rajkumardas2701 <41339878+rajkumardas2701@users.noreply.github.com>
2021-03-25 14:51:47 +0530

    Fix linter error in NavBar test

--- src/__tests__/NavBar.test.js
+++ src/__tests__/NavBar.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import Navbar from '../layouts/Navbar';
+import Navbar from '../layouts/NavBar';
 
 afterEach(cleanup);
 
